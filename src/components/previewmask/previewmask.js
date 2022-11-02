a9deec10c3c9a3b00c5b712fc0f62482a26c3ac8
yihongli@qq.com <yihongli@qq.com>
2022-11-02 20:00:47 +0800

    previewmask组件添加拖拽功能

--- src/components/previewmask/previewmask.js
+++ src/components/previewmask/previewmask.js
@@ -3,6 +3,7 @@ import PubSub from 'pubsub-js';
 import './previewmask.css';
 import jQuery from 'jquery';
 let ratio = 1;
+let ismouseup = false;
 
 export default function PreviewMask(props) {
     let $ = jQuery;
@@ -11,6 +12,9 @@ export default function PreviewMask(props) {
     let [urls, setUrls] = useState([]);
     let [src, setSrc] = useState('');
     let [scaleRatio, setScaleRatio] = useState(1);
+    let [transX, setTransX] = useState(0);
+    let [transY, setTransY] = useState(0);
+    let throttleMove;
 
     useEffect(() => {
         PubSub.subscribe('showMask', (_, data) => {
@@ -21,17 +25,16 @@ export default function PreviewMask(props) {
             onImgLoad(data.urls[data.idx]);
         });
         PubSub.subscribe('loadNormal', (_, src) => {
+            setTransX((transX = 0));
+            setTransY((transY = 0));
             setScaleRatio((ratio = 1));
             document.querySelector('.preview-img').classList.add('no-trans');
             setSrc(src);
         });
-        document.addEventListener('click', e => {
-            if (e.target.id === 'preview-mask') {
-                setShowMask(false);
-                e.stopPropagation();
-            }
-        });
-        document.addEventListener('wheel', throttle(scaleImg, 300));
+        document.body.onmousedown = function () {
+            return false;
+        };
+        document.addEventListener('wheel', throttle(scaleImg, 200));
         return () => {};
     }, []);
 
@@ -46,9 +49,16 @@ export default function PreviewMask(props) {
         });
     }
 
-    function closeMask() {
-        setShowMask(false);
-        setScaleRatio((ratio = 1));
+    function closeMask(e) {
+        if (ismouseup) {
+            return;
+        }
+        if (e.target.id === 'preview-mask' || e.target.id === 'close') {
+            setShowMask(false);
+            setScaleRatio((ratio = 1));
+            setTransX((transX = 0));
+            setTransY((transY = 0));
+        }
     }
 
     function preImg() {
@@ -109,6 +119,10 @@ export default function PreviewMask(props) {
             } else {
                 ratio -= 8;
             }
+            if (ratio === 1) {
+                setTransX((transX = 0));
+                setTransY((transY = 0));
+            }
             setScaleRatio(ratio);
         }
     }
@@ -121,58 +135,123 @@ export default function PreviewMask(props) {
     function shrink() {
         document.querySelector('.preview-img') && document.querySelector('.preview-img').classList.remove('no-trans');
         setScaleRatio((ratio = 1));
+        setTransX((transX = 0));
+        setTransY((transY = 0));
     }
 
     // 节流
-    function throttle(fn, wait) {
-        let timer;
+    function throttle(fn, wait, ...args) {
+        let pre = 0;
         return function (e) {
-            if (!timer) {
-                timer = setTimeout(() => {
-                    fn.call(this, e);
-                    timer = null;
-                }, wait);
+            // 事件的回调函数
+            let now = new Date();
+            if (now - pre > wait) {
+                fn.call(this, e, ...args);
+                pre = now;
             }
         };
     }
 
+    function mousedown(e) {
+        document.querySelector('.preview-img').classList.remove('no-trans');
+        // 获取元素当前的偏移量
+        let tx = transX;
+        let ty = transY;
+        // 记录鼠标的起始位置
+        let startX = e.clientX;
+        let startY = e.clientY;
+        throttleMove = throttle(move, 100, tx, ty, startX, startY);
+        document.addEventListener('mousemove', throttleMove);
+        document.addEventListener('mouseup', up);
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    function move(e, ...args) {
+        setTransX((transX = args[0] + (e.clientX - args[2]) / ratio));
+        setTransY((transY = args[1] + (e.clientY - args[3]) / ratio));
+        e.stopPropagation();
+    }
+
+    function up(e) {
+        document.querySelector('.preview-img').classList.add('no-trans');
+        let borderX, borderY;
+        if (ratio > 1) {
+            if ($('.preview-img').width() * ratio < window.innerWidth) {
+                borderX = (window.innerWidth - $('.preview-img').width() * ratio) / 2 / ratio;
+            } else {
+                borderX = ($('.preview-img').width() * ratio - window.innerWidth) / 2 / ratio;
+            }
+            borderY = ($('.preview-img').height() * (ratio - 1)) / 2 / ratio;
+            if (transX >= borderX) {
+                // 向右拖拽
+                transX = borderX;
+            }
+            if (transX <= -borderX) {
+                // 向左拖拽
+                transX = -borderX;
+            }
+            if (transY >= borderY) {
+                // 向下拖拽
+                transY = borderY;
+            }
+            if (transY <= -borderY) {
+                // 向上拖拽
+                transY = -borderY;
+            }
+        } else {
+            transX = 0;
+            transY = 0;
+        }
+        setTransX(transX);
+        setTransY(transY);
+        e.stopPropagation();
+        setShowMask((showMask = true));
+        ismouseup = true;
+        setTimeout(function () {
+            ismouseup = false;
+        }, 200);
+        document.removeEventListener('mousemove', throttleMove);
+        document.removeEventListener('mouseup', up);
+    }
+
     let dom = showMask ? (
-        <div id='preview-mask' className='preview-mask h-v-full w-v-full fixed'>
+        <div id='preview-mask' className='preview-mask h-v-full w-v-full fixed' onClick={closeMask}>
             <img
+                id='preview-img'
                 className='preview-img absolute absolute-center pointer'
                 src={src}
-                alt=''
-                style={{ transform: `scale3d(${scaleRatio}, ${scaleRatio}, 1)` }}
+                alt='translate3d(${moveX}px, ${moveY}px, 0)'
+                onMouseDown={mousedown}
+                style={{ transform: `scale3d(${scaleRatio}, ${scaleRatio}, 1) translate3d(${transX}px, ${transY}px, 0px)` }}
             />
-            <div className='mask-head fixed w-v-full'>
-                <span className='progress relative'>
-                    {current + 1} / {urls.length}
-                </span>
-                <span className='iconfont icon-24gl-delete' onClick={closeMask}></span>
-                <span id='fangda' className='iconfont icon-fangda' onClick={throttle(grow, 300)}></span>
-                <span id='suoxiao' className='iconfont icon-suoxiao' onClick={throttle(shrink, 300)}></span>
+            <span className='progress relative'>
+                {current + 1} / {urls.length}
+            </span>
+            <div className='mask-head fixed'>
+                <span id='close' className='iconfont icon-24gl-delete' onClick={closeMask}></span>
+                <span id='fangda' className='iconfont icon-fangda' onClick={throttle(grow, 200)}></span>
+                <span id='suoxiao' className='iconfont icon-suoxiao' onClick={throttle(shrink, 200)}></span>
                 <span className='iconfont icon-rotate-right'></span>
                 <span className='iconfont icon-rotate-left'></span>
             </div>
-            <div className='toggle-btn fixed w-v-full'>
-                <div
-                    className='flex-center margin-l-10'
-                    onClick={preImg}
-                    style={{
-                        cursor: current === 0 ? 'no-drop' : 'pointer',
-                        color: current === 0 ? 'rgba(204, 204, 204, 0.5)' : 'var(--w-color-gray-7)'
-                    }}>
-                    <span className='iconfont icon-arrow-left-bold'></span>
-                </div>
-                <div
-                    className='flex-center margin-r-10'
-                    onClick={nextImg}
-                    style={{
-                        cursor: current === urls.length - 1 ? 'no-drop' : 'pointer',
-                        color: current === urls.length - 1 ? 'rgba(204, 204, 204, 0.5)' : 'var(--w-color-gray-7)'
-                    }}>
-                    <span className='iconfont icon-arrow-right-bold'></span>
-                </div>
+            <div
+                className='toggle-btn left-btn margin-l-10 fixed'
+                onClick={preImg}
+                style={{
+                    cursor: current === 0 ? 'no-drop' : 'pointer',
+                    color: current === 0 ? 'rgba(204, 204, 204, 0.5)' : 'var(--w-color-gray-7)'
+                }}>
+                <span className='iconfont icon-arrow-left-bold'></span>
+            </div>
+            <div
+                className='toggle-btn right-btn margin-r-10 fixed'
+                onClick={nextImg}
+                style={{
+                    cursor: current === urls.length - 1 ? 'no-drop' : 'pointer',
+                    color: current === urls.length - 1 ? 'rgba(204, 204, 204, 0.5)' : 'var(--w-color-gray-7)'
+                }}>
+                <span className='iconfont icon-arrow-right-bold'></span>
             </div>
             <div className='mask-foot fixed w-v-full'></div>
         </div>
